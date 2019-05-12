8fbe7ecc6e91698d8f165781c042dc1f0201c1a3
Poetic Justice <mcguffen@hanshanyang.xyz>
2019-05-12 14:50:12 +0800

    创建分类

--- admin/src/router.js
+++ admin/src/router.js
@@ -1,7 +1,9 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Home from './views/Home.vue'
+
 import Main from './views/Main.vue'
+import CategoryEdit from './views/CategoryEdit.vue'
+
 
 
 Vue.use(Router)
@@ -11,7 +13,10 @@ export default new Router({
     {
       path: '/',
       name: 'main',
-      component: Main
+      component: Main,
+      children: [
+        {path: '/categories/create', component: CategoryEdit}
+      ]
     },
     
   ]
