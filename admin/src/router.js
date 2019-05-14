62116eb442e08525fc49eaf0443490ac3467423b
Poetic Justice <mcguffen@hanshanyang.xyz>
2019-05-14 17:56:24 +0800

    把分类列表加入路由

--- admin/src/router.js
+++ admin/src/router.js
@@ -4,6 +4,8 @@ import Router from 'vue-router'
 import Main from './views/Main.vue'
 // 引入新创建的页面 然后去修改写死的main.vue内容 
 import CategoryEdit from './views/CategoryEdit.vue'
+import CategoryList from './views/CategoryList.vue'
+
 
 
 
@@ -17,7 +19,9 @@ export default new Router({
       component: Main,
       // 添加子路由
       children: [
-        {path: '/categories/create', component: CategoryEdit}
+        {path: '/categories/create', component: CategoryEdit},
+        {path: '/categories/list', component: CategoryList},
+
       ]
     },
     
