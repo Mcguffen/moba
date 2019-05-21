8f6451c33a4203801861337b7924c467f7b802f1
Poetic Justice <mcguffen@hanshanyang.xyz>
2019-05-21 14:28:23 +0800

    添加英雄模型数据字段定义的注释

--- server/models/Hero.js
+++ server/models/Hero.js
@@ -7,25 +7,31 @@ const schema = new mongoose.Schema({
   title: { type: String },
   // 这个英雄属于什么类型 有可能多个类型哦 比如肉盾 先手
   categories: [{ type: mongoose.SchemaTypes.ObjectId, ref: 'Category' }],
-  // 
+  // 得分 
   scores: {
     difficult: { type: Number },
     skills: { type: Number },
     attack: { type: Number },
     survive: { type: Number },
   },
-
+  // 技能
   skills: [{
     icon: { type: String },
     name: { type: String },
     description: { type: String },
     tips: { type: String },
   }],
+  // 顺风装备推荐
   items1: [{ type: mongoose.SchemaTypes.ObjectId, ref: 'Item' }],
+  // 逆风装备推荐
   items2: [{ type: mongoose.SchemaTypes.ObjectId, ref: 'Item' }],
+  // 使用技巧
   usageTips: { type: String },
+  // 对线技巧
   battleTips: { type: String },
+  // 打团思路
   teamTips: { type: String },
+  // 搭档，配合阵容
   partners: [{
     hero: { type: mongoose.SchemaTypes.ObjectId, ref: 'Hero' },
     description: { type: String },
