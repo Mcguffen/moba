10b7a285e3fcb7839d8a47e6d7c5d85105f69d6e
Poetic Justice <mcguffen@hanshanyang.xyz>
2019-05-28 10:30:24 +0800

    添加注释
    为了服务端验证登陆我们需要对每一个接口加一层中间件来验证登陆。下一步我们就这么做。

--- server/routes/admin/index.js
+++ server/routes/admin/index.js
@@ -4,20 +4,24 @@ module.exports = app => {
     mergeParams: true
   })
 
+  // 创建资源
   router.post('/', async (req, res) => {
     const model = await req.Model.create(req.body)
     res.send(model)
   })
+  // 更改资源
   router.put('/:id', async (req, res) => {
     const model = await req.Model.findByIdAndUpdate(req.params.id, req.body)
     res.send(model)
   })
+  // 删除资源
   router.delete('/:id', async (req, res) => {
     await req.Model.findByIdAndDelete(req.params.id)
     res.send({
       success: true
     })
   })
+  // 资源列表
   router.get('/', async (req, res) => {
     const queryOptions = {}
     if (req.Model.modelName === 'Category') {
