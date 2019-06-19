9246389b1359e9e49fc59eab0eec009cb8dbe313
mcguffen <504177380@qq.com>
2019-06-19 17:24:13 +0800

    首页新闻 后台数据录入

--- server/index.js
+++ server/index.js
@@ -16,6 +16,8 @@ require('./plugins/db')(app)
 // 引用路由文件下./routes/admin的函数使用他，然后传给他app参数，意思就是调用函数传参数给他，那么路由文件下的函数就可以使用外部的app啦
 require('./routes/admin')(app)
 
+require('./routes/web')(app)
+
 // 监听3000端口 分类路由需要我们单独写在一个文件routes下，按前后台分，这么方便管理。
 app.listen(3000, () => {
   console.log('http://localhost:3000');
