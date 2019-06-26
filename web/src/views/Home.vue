e888773a70dbdc0f554cd648ee47500b6a8bce45
mcguffen <504177380@qq.com>
2019-06-26 14:14:06 +0800

    新闻详情页

--- web/src/views/Home.vue
+++ web/src/views/Home.vue
@@ -15,28 +15,18 @@
     <!-- end of swiper -->
         <!-- end of swiper -->
 
-    <div class="nav-icons bg-white mt-3 text-center pt-3 text-dark-1">
-      <div class="d-flex flex-wrap">
-        <div class="nav-item mb-3" v-for="n in 10" :key="n">
-          <i class="sprite sprite-news"></i>
-          <div class="py-2">爆料站</div>
-        </div>
-      </div>
-      <div class="bg-light py-2 fs-sm">
-        <i class="sprite sprite-arrow mr-1"></i>
-        <span>收起</span>
-      </div>
-    </div>
-    <!-- end of nav icons -->
-
     <m-list-card icon="menu1" title="新闻资讯" :categories="newsCats">
       <template #items="{category}">
-        <div class="py-2 fs-lg d-flex" v-for="(news, i) in category.newsList" :key="i">
+        <router-link 
+        tag="div"
+        :to="`/articles/${news._id}`"
+        class="py-2 fs-lg d-flex" 
+        v-for="(news, i) in category.newsList" :key="i">
           <span class="text-info">[{{news.categoryName}}]</span>
           <span class="px-2">|</span>
           <span class="flex-1 text-dark-1 text-ellipsis pr-2">{{news.title}}</span>
           <span class="text-grey-1 fs-sm">{{news.createdAt | date}}</span>
-        </div>
+        </router-link>
       </template>
     </m-list-card>
 
