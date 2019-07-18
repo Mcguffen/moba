b063ef508be68484f177468ceb13c1eea4407fa9
mcguffen <504177380@qq.com>
2019-07-18 18:26:10 +0800

    生产环境编译完成

--- server/index.js
+++ server/index.js
@@ -11,6 +11,10 @@ app.use(express.json())
 // 静态文件托管 在nodejs里面 使用下面的 可以将莫个文件下的文件静态文件托管 
 app.use('/uploads', express.static(__dirname + '/uploads'))
 
+app.use('/admin', express.static(__dirname + '/admin'))
+
+app.use('/', express.static(__dirname + '/web'))
+
 // 同样的 和路由地址管理一样 我们单独讲这个数据库单独放在/plugins/db文件下 方法和之前routes一样 数据库链接上后 就需要模型
 require('./plugins/db')(app)
 // 引用路由文件下./routes/admin的函数使用他，然后传给他app参数，意思就是调用函数传参数给他，那么路由文件下的函数就可以使用外部的app啦
