2539368256fa1a052553babc2f7fe4122926c0b1
mcguffen <504177380@qq.com>
2019-07-18 16:26:48 +0800

    fix upload

--- admin/src/main.js
+++ admin/src/main.js
@@ -3,12 +3,27 @@ import App from './App.vue'
 import './plugins/element.js'
 import router from './router'
 
+import './style.css'
+
 Vue.config.productionTip = false
 
-// 引入模块  下面可以挂载到Vue上去 以后用更方便
 import http from './http'
-// 加载到Vue实例原型上去后 都可以使用this.$http来调用http模块 我们回到CategoryEdit.vue中的method中现学现卖
 Vue.prototype.$http = http
+
+Vue.mixin({
+  computed: {
+    uploadUrl(){
+      return this.$http.defaults.baseURL + '/upload'
+    }
+  },
+  methods: {
+    getAuthHeaders(){
+      return {
+        Authorization: `Bearer ${localStorage.token || ''}`
+      }
+    }
+  }
+})
 
 new Vue({
   router,
